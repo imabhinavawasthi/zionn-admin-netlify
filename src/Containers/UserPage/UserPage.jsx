282b1ee9a8e8f2c87e85c0d82aa5acb0e90a400a
imabhinavawasthi <awasthiabhinav744@gmail.com>
2022-08-31 18:17:59 +0530

    userpage check

--- src/Containers/UserPage/UserPage.jsx
+++ src/Containers/UserPage/UserPage.jsx
@@ -10,24 +10,15 @@ import { useNavigate } from "react-router";
 const UserPage = () => {
   const curruser = localStorage.getItem("user");
   const navigate = useNavigate();
-  let checkadmin = false;
-  const userobj = JSON.parse(localStorage.getItem('user'));
   if (curruser !== null) {
     setTimeout(() => {
       navigate("/");
     }, 500)
   }
-  else if (userobj?.email == "bhanu@zionn.trade") {
-    checkadmin = true;
-  }
-  else {
-    setTimeout(() => {
-      navigate("/");
-    }, 1000)
-  }
+
   const params = useParams();
   const [userdetails, setUserDetails] = useState([])
-  
+  const userobj = JSON.parse(localStorage.getItem('user'));
 
   useEffect(() => {
 
@@ -42,79 +33,77 @@ const UserPage = () => {
     f()
   }, []);
   return (
-    <>
-      {checkadmin ? (<div className='container p-2'>
-        <h1 className="mb-5 mt-3 head-user-page-css">
-          User Details <NavLink to=""><h5 style={{ color: '#000' }}><i class="bi bi-pen-fill"></i>edit</h5></NavLink>
-        </h1>
-        <div className="heading-cp-css head-font">onboarding</div>
-        <div className="container-sm  main-con mb-5">
-          <RowOnboarding
-            a="name"
-            b={userdetails[0]?.user_name}
-          />
-          <RowOnboarding
-            a="email"
-            b={userdetails[0]?.email}
-          />
-          <RowOnboarding
-            a="phone #"
-            b={userdetails[0]?.phone}
-          />
-          <RowOnboarding
-            a="unique id"
-            b={params.uid}
-          />
-          <RowOnboarding
-            a="company"
-            b={userdetails[0]?.curr_employer}
-          />
-          <RowOnboarding
-            a="designation"
-            b={userdetails[0]?.designation}
-          />
-          <RowOnboarding
-            a="tenure"
-            b={userdetails[0]?.tenure}
-          />
-        </div>
+    <div className='container p-2'>
+      <h1 className="mb-5 mt-3 head-user-page-css">
+        User Details <NavLink to=""><h5 style={{ color: '#000' }}><i class="bi bi-pen-fill"></i>edit</h5></NavLink>
+      </h1>
+      <div className="heading-cp-css head-font">onboarding</div>
+      <div className="container-sm  main-con mb-5">
+        <RowOnboarding
+          a="name"
+          b={userdetails[0]?.user_name}
+        />
+        <RowOnboarding
+          a="email"
+          b={userdetails[0]?.email}
+        />
+        <RowOnboarding
+          a="phone #"
+          b={userdetails[0]?.phone}
+        />
+        <RowOnboarding
+          a="unique id"
+          b={params.uid}
+        />
+        <RowOnboarding
+          a="company"
+          b={userdetails[0]?.curr_employer}
+        />
+        <RowOnboarding
+          a="designation"
+          b={userdetails[0]?.designation}
+        />
+        <RowOnboarding
+          a="tenure"
+          b={userdetails[0]?.tenure}
+        />
+      </div>
 
-        <div>
-          <div className="heading-cp-css head-font">sell/buy</div>
-          <div className="container-sm  main-con">
-            <div className="row g-4">
-              <div className="col-4">
-                <div className="cell-wide cell purple-b">
-                  <strong>company</strong>
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="cell-mid cell purple-b">
-                  <strong>inventory</strong>
-                </div>
-              </div>
-              <div className="col-3">
-                <div className="cell-mid cell purple-b">
-                  <strong>ownership</strong>
-                </div>
+      <div>
+        <div className="heading-cp-css head-font">sell/buy</div>
+        <div className="container-sm  main-con">
+          <div className="row g-4">
+            <div className="col-4">
+              <div className="cell-wide cell purple-b">
+                <strong>company</strong>
               </div>
             </div>
-            {userdetails.map((detail) => {
-              return (
-                <div>
-                  <RowUserPage
-                    a={detail?.c_name}
-                    b={detail?.no_of_shares}
-                    c={detail?.doc_url}
-                  />
-                </div>
-              )
-
-            })}
+            <div className="col-4">
+              <div className="cell-mid cell purple-b">
+                <strong>inventory</strong>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="cell-mid cell purple-b">
+                <strong>ownership</strong>
+              </div>
+            </div>
           </div>
+          {userdetails.map((detail) => {
+            return (
+              <div>
+                <RowUserPage
+                  a={detail?.c_name}
+                  b={detail?.no_of_shares}
+                  c={detail?.doc_url}
+                />
+              </div>
+            )
+
+          })}
         </div>
-      </div>):(<>you are not authorised to access this page</>)}
-    </>
+      </div>
+    </div>
   )
 }
 
