a2470584460d272e4dba564ebb523102208cc5de
imabhinavawasthi <awasthiabhinav744@gmail.com>
2022-09-01 23:24:00 +0530

    baseurl

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    // baseURL: 'https://zionn-stage.herokuapp.com/'
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://zionn-stage.herokuapp.com/'
+    // baseURL: 'http://localhost:8080/'
 })
 
 API.interceptors.request.use((req) => {
