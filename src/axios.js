1605f91360fe4adf9abf95c385139c03384b2e5f
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-09-17 00:42:36 +0530

    baseurl to prod

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const API = axios.create({
 //     baseURL: 'https://zionn-stage.herokuapp.com/'
-    //   baseURL:'https://zionn-prod.herokuapp.com/'
-    baseURL: 'http://localhost:8080/'
+      baseURL:'https://zionn-prod.herokuapp.com/'
+    // baseURL: 'http://localhost:8080/'
 }) 
 
 API.interceptors.request.use((req) => {
