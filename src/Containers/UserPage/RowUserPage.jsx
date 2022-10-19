5f4622fb96f30703008e81ad9da462df502c2d68
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2022-10-19 19:40:59 +0530

    prod check

--- src/Containers/UserPage/RowUserPage.jsx
+++ src/Containers/UserPage/RowUserPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { NavLink } from "react-router-dom";
 import * as api from "../../axios"
 
 const RowUserPage = (props) => {
-console.log(props);
+  console.log(props);
   var date = new Date(parseInt(props.e));
   var dateToStr = date.toUTCString().split(' ');
   var cleanDate = dateToStr[1] + ' ' + dateToStr[2] + ' ' + dateToStr[3] + ' ' + dateToStr[0] + ' ' + dateToStr[4];
@@ -14,20 +14,20 @@ console.log(props);
     <div>
       <div className="row mt-2 gr-4">
         <div className="col-4">
-<div className="row">
-<div className="col-5 ">
-          <div className="cell-wide cell"><a style={{ textDecoration: 'none ', color: '#7b61ff' }} target="___blank" href={`https://beta.zionn.trade/company/${props.a}`}>{props.a || <Skeleton width={80} height={15} />}{props.a ? (<i className="bi bi-arrow-up-right" />) : (<></>)}</a></div>
-        </div>
-        <div className="col-4 ">
-          <div className="cell-mid cell">{props.b || <Skeleton width={80} height={15} />}</div>
-        </div>
-        <div className="col-3 ">
-          <div className="cell-mid cell">{props.sell||props.buy|| <Skeleton width={80} height={15} />}</div>
-        </div>
-</div>
+          <div className="row">
+            <div className="col-5 ">
+              <div className="cell-wide cell"><a style={{ textDecoration: 'none ', color: '#7b61ff' }} target="___blank" href={`https://beta.zionn.trade/company/${props.a}`}>{props.a || <Skeleton width={80} height={15} />}{props.a ? (<i className="bi bi-arrow-up-right" />) : (<></>)}</a></div>
+            </div>
+            <div className="col-4 ">
+              <div className="cell-mid cell">{props.b || <Skeleton width={80} height={15} />}</div>
+            </div>
+            <div className="col-3 ">
+              <div className="cell-mid cell">{props.sell || props.buy || <Skeleton width={80} height={15} />}</div>
+            </div>
+          </div>
         </div>
         <div className="col-1">
-          <div className="cell-mid cell">{props.d|| <Skeleton width={80} height={15} />}</div>
+          <div className="cell-mid cell">{props.d || <Skeleton width={80} height={15} />}</div>
         </div>
         <div className="col-3 ">
           <div className="cell-mid cell">{props.e ? cleanDate : <Skeleton width={80} height={15} />}</div>
